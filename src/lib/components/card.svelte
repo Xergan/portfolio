1502682089056a1c0fd2803d3c4e3179fe6f7fce
Xergan <nicolaspqd10@gmail.com>
2025-02-10 12:06:12 +0100

    card update

--- src/lib/components/card.svelte
+++ src/lib/components/card.svelte
@@ -1,18 +1,38 @@
+<script lang="ts">
+	import GithubIcon from "$lib/svgs/github_icon.svelte";
+	import ItchIcon from "$lib/svgs/itch_icon.svelte";
+
+  let {title, description, image, alt, isNew = false, tags = []} = $props();
+</script>
+
 <div class="card bg-base-100 w-96 shadow-xl">
     <figure>
       <img
-        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-        alt="Shoes" />
+        src={image}
+        alt={alt} />
     </figure>
     <div class="card-body">
       <h2 class="card-title">
-        Shoes!
+        {title}
+        {#if isNew}
         <div class="badge badge-secondary">NEW</div>
+        {/if}
       </h2>
-      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <p>{description}</p>
+      <div class="card-actions justify-start">
+        {#each tags as tag}
+          <div class="badge badge-outline">{tag}</div>
+        {/each}
+      </div>
       <div class="card-actions justify-end">
-        <div class="badge badge-outline">Fashion</div>
-        <div class="badge badge-outline">Products</div>
+        <a class="btn btn-secondary" href="/">
+          <ItchIcon/>
+          Itch.io
+        </a>
+        <a class="btn btn-primary" href="/">
+          <GithubIcon/>
+          Github
+        </a>
       </div>
     </div>
 </div>
--- src/routes/games/+page.svelte
+++ src/routes/games/+page.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
     import Card from '$lib/components/card.svelte';
 	import MenuBar from '$lib/components/menu_bar.svelte';
-import type { PageData } from './$types';
-
-    let { data }: { data: PageData } = $props();
 </script>
 
 <MenuBar/>
-<Card></Card>
+
+<div class="pt-28 pb-4 w-full justify-center flex">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
+        <Card 
+            title="Shoes!" 
+            image="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" 
+            alt="shoes" 
+            description="If a dog chews shoes whose shoes does he choose?" 
+            />
+    </div>
+</div>
