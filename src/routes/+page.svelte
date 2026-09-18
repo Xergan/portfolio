df0e7412909a70947b1cd20ee5bda365e8f57051
Xergan <nicolaspqd10@gmail.com>
2026-09-18 11:11:07 +0200

    Refactor hero section layout in page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,8 +10,8 @@
 <div class="fixed h-full w-full bg-cover bg-top" style="background-image: url(/images/background.png);">
   <div class="h-full w-full backdrop-blur-sm bg-base-300 bg-opacity-70"></div>
 </div>
-<div class="hero min-h-screen top-1">
-<div class="hero-content lg:w-2/3 w-full min-h-screen">
+<div class="hero min-h-screen">
+<div class="hero-content lg:w-2/3 w-full min-h-screen pb-12">
     <div class="hero-content flex-col lg:flex-row-reverse">
       <img
         src="https://github.com/xergan.png"
